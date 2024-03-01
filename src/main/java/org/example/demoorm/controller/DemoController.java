e56378bc1832d30a223b4685ad6ca8900ad510bd
VuKieuAnh <vukieuanh.hnue@gmail.com>
2024-03-01 15:53:09 +0700

    add file

---
+++ src/main/java/org/example/demoorm/controller/DemoController.java
@@ -0,0 +1,22 @@
+package org.example.demoorm.controller;
+
+import org.example.demoorm.model.Customer;
+import org.example.demoorm.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class DemoController {
+    @Autowired
+    private ICustomerService customerService;
+    @GetMapping("/demo")
+    public String demo(Model model){
+        List<Customer> customers = customerService.findAll();
+        model.addAttribute("c", customers);
+        return "index";
+    }
+}
---
+++ src/main/java/org/example/demoorm/service/ICustomerService.java
@@ -0,0 +1,14 @@
+package org.example.demoorm.service;
+import org.example.demoorm.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void remove(int id);
+}
